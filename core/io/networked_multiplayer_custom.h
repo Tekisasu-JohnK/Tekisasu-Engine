90122833dad5233182af7ea4f8e2594b5889c08c
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2022-08-05 18:26:36 -0500

    3.5.0 backport - core

--- core/io/networked_multiplayer_custom.h
+++ core/io/networked_multiplayer_custom.h
@@ -1,12 +1,12 @@
 /*************************************************************************/
-/*  copymem.h                                                            */
+/*  networked_multiplayer_custom.h                                       */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
 /*                      https://godotengine.org                          */
 /*************************************************************************/
-/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
-/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
+/* Copyright (c) 2007-2022 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2022 Godot Engine contributors (cf. AUTHORS.md).   */
 /*                                                                       */
 /* Permission is hereby granted, free of charge, to any person obtaining */
 /* a copy of this software and associated documentation files (the       */
@@ -28,23 +28,60 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#ifndef COPYMEM_H
-#define COPYMEM_H
+#ifndef NETWORKED_MULTIPLAYER_CUSTOM_H
+#define NETWORKED_MULTIPLAYER_CUSTOM_H
 
-#include "core/typedefs.h"
+#include "core/io/networked_multiplayer_peer.h"
 
-#ifdef PLATFORM_COPYMEM
+class NetworkedMultiplayerCustom : public NetworkedMultiplayerPeer {
+	GDCLASS(NetworkedMultiplayerCustom, NetworkedMultiplayerPeer);
 
-#include "platform_copymem.h" // included from platform/<current_platform>/platform_copymem.h"
+protected:
+	int self_id;
+	TransferMode transfer_mode;
+	ConnectionStatus connection_status;
+	bool refusing_new_connections;
+	int target_id;
+	int max_packet_size;
 
-#else
+	struct Packet {
+		PoolVector<uint8_t> data;
+		int from;
+	};
 
-#include <string.h>
+	List<Packet> incoming_packets;
 
-#define copymem(to, from, count) memcpy(to, from, count)
-#define zeromem(to, count) memset(to, 0, count)
-#define movemem(to, from, count) memmove(to, from, count)
+	Packet current_packet;
 
-#endif
+	static void _bind_methods();
 
-#endif
+public:
+	NetworkedMultiplayerCustom();
+	~NetworkedMultiplayerCustom();
+
+	// PacketPeer.
+	Error get_packet(const uint8_t **r_buffer, int &r_buffer_size);
+	Error put_packet(const uint8_t *p_buffer, int p_buffer_size);
+	int get_available_packet_count() const;
+	int get_max_packet_size() const;
+
+	// NetworkedMultiplayerPeer.
+	void set_transfer_mode(TransferMode p_mode);
+	TransferMode get_transfer_mode() const;
+	void set_target_peer(int p_peer);
+	int get_packet_peer() const;
+	bool is_server() const;
+	void poll();
+	int get_unique_id() const;
+	void set_refuse_new_connections(bool p_enable);
+	bool is_refusing_new_connections() const;
+	ConnectionStatus get_connection_status() const;
+
+	// Custom methods.
+	void initialize(int p_self_id);
+	void set_max_packet_size(int p_max_packet_size);
+	void set_connection_status(ConnectionStatus p_connection_status);
+	void deliver_packet(const PoolByteArray &p_data, int p_from_peer_id);
+};
+
+#endif // NETWORKED_MULTIPLAYER_CUSTOM_H
