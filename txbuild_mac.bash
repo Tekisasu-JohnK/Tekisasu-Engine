ced43b5d783e2b99a4b3856b529564c57c9f06af
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2022-11-05 15:21:33 -0500

    fix txbuild_mac.bash scons platform

--- txbuild_mac.bash
+++ txbuild_mac.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 stage=$1
 if [[ "$stage" == "1" ]]; then
-	scons platform=osx arch=arm64 --jobs=$(sysctl -n hw.logicalcpu) tools=yes
-	scons platform=osx arch=x86_64 --jobs=$(sysctl -n hw.logicalcpu) tools=yes
+	scons platform=macos arch=arm64 --jobs=$(sysctl -n hw.logicalcpu) tools=yes
+	scons platform=macos arch=x86_64 --jobs=$(sysctl -n hw.logicalcpu) tools=yes
 	lipo -create bin/godot.macos.editor.x86_64 bin/godot.macos.editor.arm64 -output bin/godot.macos.editor.universal
 elif [[ "$stage" == "2" ]]; then
 	cd bin
