fef91ccdf1265ace8aa375abed8c4bc30d474bf9
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2022-11-19 19:59:18 -0600

    4.0b5 - root (fixes)

--- methods.py
+++ methods.py
@@ -69,7 +69,7 @@ def add_module_version_string(self, s):
 
 
 def get_version_info(module_version_string="", silent=False):
-    build_name = "custom_build"
+    build_name = "txbuild"
     if os.getenv("BUILD_NAME") != None:
         build_name = str(os.getenv("BUILD_NAME"))
         if not silent:
