9a72277bb5d19ad284f6f3ed58d58dfeab01bda4
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2023-11-30 09:45:04 -0600

    root - rebase

--- version.py
+++ version.py
@@ -1,10 +1,10 @@
-short_name = "tekisasuengine"
-name = "Tekisasu Engine"
+short_name = "godot"
+name = "Godot Engine"
 major = 4
-minor = 1
+minor = 2
 patch = 0
-status = "p1"
+status = "stable"
 module_config = ""
 year = 2023
-website = "https://dev.tekisasu.com"
-docs = "latest"
+website = "https://godotengine.org"
+docs = "4.2"
